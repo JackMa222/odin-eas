28cfb9b1a94b8999ee2ed55c25a54264b8686df3
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-31 10:21:03 +1100

    Remove children from container div

--- eas.js
+++ eas.js
@@ -1,6 +1,10 @@
 body = document.querySelector("#container");
 
 function createGrid(size) {
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+
     for (let i = 0; i < size; i++) {
         const newRow = document.createElement("div");
         newRow.classList.add("row");
@@ -20,6 +24,9 @@ function createGrid(size) {
 newGridButton = document.querySelector("#newGridButton");
 newGridButton.addEventListener("click", () => {
     size = Number(prompt("Size of grid?"))
+    if (size > 100) {
+        size = 100;
+    }
     createGrid(size);
 });
 
